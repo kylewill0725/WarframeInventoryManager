69604308cd7075b52d3a958e1bac5b9427c2e4e5
kylewill0725 <kylewill0725@outlook.com>
2018-09-13 10:33:22 -0500

    Methods and classes needed to implement tail recursion in python have been extracted to their own file.

--- image_parser.py
+++ image_parser.py
@@ -1,3 +1,5 @@
+from Recurse import recurse, tail_recursive
+
 try:
     import Image, ImageGrab
 except ImportError:
@@ -8,29 +10,6 @@ import win32api
 from win32api import *
 import time
 import math
-
-
-class Recurse(Exception):
-    def __init__(self, *args, **kwargs):
-        self.args = args
-        self.kwargs = kwargs
-
-
-def recurse(*args, **kwargs):
-    raise Recurse(*args, **kwargs)
-
-
-def tail_recursive(f):
-    def decorated(*args, **kwargs):
-        while True:
-            try:
-                return f(*args, **kwargs)
-            except Recurse as r:
-                args = r.args
-                kwargs = r.kwargs
-                continue
-
-    return decorated
 
 
 class TessData:
