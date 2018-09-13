c6f2e427cc6d5c26b098713d4fcc04f0e7e8e0a6
kylewill0725 <kylewill0725@outlook.com>
2018-09-12 22:07:13 -0500

    Demo is now working. Image parsing is incomplete.

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+try:
+    import Image, ImageGrab
+except ImportError:
+    from PIL import Image, ImageGrab, ImageFile, ImageFilter
+from image_parser import parseItemPage
+
+
 def image_parse_test():
-    pass
+    demoPage = Image.open("example_data/inventory_pics/test0.png")
+    parseItemPage(demoPage)
+
+
+if __name__ == '__main__':
+    image_parse_test()
